c639dcdb208d05e73b7379c585ec9899be6fb593
ilnar4888 <25gaisarov@gmail.com>
2017-10-16 09:45:36 +0300

    test

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Feedback do
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :text }
+
+  describe "#name" do
+    let(:feedback) { build(:feedback) }
+    let(:name) { "#{feedback.name}" }
+    let(:email) { "#{feedback.email}" }
+    let(:text) { "#{feedback.text}" }
+
+    describe "#name" do
+      subject { feedback.name }
+
+      it { is_expected.to eq(name) }
+    end
+
+    describe "#email" do
+      subject { feedback.email }
+
+      it { is_expected.to eq(email) }
+    end
+
+    describe "#full_name_with_email" do
+      subject { feedback.text }
+
+      it { is_expected.to eq(text) }
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,10 +4,17 @@ require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "shoulda/matchers"
+require "capybara/rspec"
+require "pundit/rspec"
 
-Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  config.include Warden::Test::Helpers
+
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/testing"]) if Rails.env.test?
+  end
 end
